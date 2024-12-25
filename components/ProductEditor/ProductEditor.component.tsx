c7be4163442248b943d3b21ecde6e868478ddd2e
Andrew Wittrock <andy@wittrock.us>
2024-12-24 22:06:36 -0600

    Add tabs to product editor

--- components/ProductEditor/ProductEditor.component.tsx
+++ components/ProductEditor/ProductEditor.component.tsx
@@ -2,7 +2,7 @@ import { Editable } from "@core/components/Editable";
 import { Label } from "@core/components/Label";
 import { onDateChange, onNumberChange } from "@core/lib/onInputChange";
 import Editor from "@uiw/react-md-editor";
-import { Card, Col, DatePicker, Radio, Row, Space, Spin, Switch } from "antd";
+import { Card, Col, DatePicker, Radio, Row, Space, Spin, Switch, Tabs } from "antd";
 import dayjs from "dayjs";
 import { ProductEditorProps } from "./ProductEditor.d";
 import styles from './ProductEditor.module.scss';
@@ -15,52 +15,64 @@ export const ProductEditorComponent = ({product, isLoading, updateNumber, update
     <Spin spinning={isLoading}>
         {!!product && <>
             <Row className={styles.productEditor} gutter={8}>
-                <Col span={12} offset={6}>
-                    <Space direction="vertical">
-                        <Space>
-                            <Switch checked={product.enabled} checkedChildren="Enabled" unCheckedChildren="Disabled" onChange={updateToggle("enabled")}/>
-                            <DatePicker value={dayjs(product.releaseDate)} onChange={onDateChange(updateString("releaseDate"))}/>
-                        </Space>
-                        <h1>
-                            <Label label="Name"><Editable value={product.name} onChange={updateString("name")}/></Label>
-                        </h1>
-                        <Space>
-                            <Radio.Group block optionType="button" buttonStyle="solid" value={product.productType} onChange={(e) => updateString("productType")(e.target.value)}>
-                                <Radio value="digital">Digital</Radio>
-                                <Radio value="grouped">Grouped</Radio>
-                            </Radio.Group>
-                            <Switch
-                                checked={product.subscriptionOnly}
-                                checkedChildren="Subscription Only"
-                                unCheckedChildren="Not Subscription Only"
-                                onChange={updateToggle("subscriptionOnly")}
-                            />
-                            <Label label="Price"><Editable value={`${product.price}`} onChange={onNumberChange(updateNumber("price"))}/></Label>
-                        </Space>
-
-                        <Label label="SKU"><Editable value={product.sku} onChange={updateString("sku")}/></Label>
-
-                        <Label label="URL">
-                            <Editable value={product.url} onChange={updateString("url")}/><br/>
-                        </Label>
-
-                        <Card title="Short Description" size="small">
-                            <Editor value={product.descriptionShort} onChange={updateString("descriptionShort")}/>
-                        </Card>
-
-                        <Card title="Description" size="small">
-                            <Editor value={product.description} onChange={updateString("description")} />
-                        </Card>
-
-                        <Label label="Meta Title"><Editable value={product.metaTitle || ""} onChange={updateString("metaTitle")}/></Label>
-                        <Label label="Meta Description"><Editable textArea value={product.metaDescription || ""} onChange={updateString("metaDescription")}/></Label>
-                        <Label label="Meta Keywords"><Editable value={product.metaKeywords || ""} onChange={updateString("metaKeywords")}/></Label>
-
-                        <ProductTagEditor productId={product.id} />
-                        <ProductMediaEditor product={product} />
-                        <RelatedProductsEditor productId={product.id} />
-                        <ProductFilesEditor productId={product.id} />
+                <Col span={24}>
+                    <Space>
+                        <Switch checked={product.enabled} checkedChildren="Enabled" unCheckedChildren="Disabled" onChange={updateToggle("enabled")}/>
+                        <DatePicker value={dayjs(product.releaseDate)} onChange={onDateChange(updateString("releaseDate"))}/>
                     </Space>
+                    <h1>
+                        <Label label="Name"><Editable value={product.name} onChange={updateString("name")}/></Label>
+                    </h1>
+                    <hr/>
+                    <Tabs tabPosition="left">
+                        <Tabs.TabPane key="details" tab="Details">
+                            <Space direction="vertical">
+                                <Space>
+                                    <Radio.Group block optionType="button" buttonStyle="solid" value={product.productType} onChange={(e) => updateString("productType")(e.target.value)}>
+                                        <Radio value="digital">Digital</Radio>
+                                        <Radio value="grouped">Grouped</Radio>
+                                    </Radio.Group>
+                                    <Switch
+                                        checked={product.subscriptionOnly}
+                                        checkedChildren="Subscription Only"
+                                        unCheckedChildren="Not Subscription Only"
+                                        onChange={updateToggle("subscriptionOnly")}
+                                    />
+                                    <Label label="Price"><Editable value={`${product.price}`} onChange={onNumberChange(updateNumber("price"))}/></Label>
+                                </Space>
+
+                                <Label label="SKU"><Editable value={product.sku} onChange={updateString("sku")}/></Label>
+
+                                <Label label="URL">
+                                    <Editable value={product.url} onChange={updateString("url")}/><br/>
+                                </Label>
+
+                                <Card title="Short Description" size="small">
+                                    <Editor value={product.descriptionShort} onChange={updateString("descriptionShort")}/>
+                                </Card>
+
+                                <Card title="Description" size="small">
+                                    <Editor value={product.description} onChange={updateString("description")} />
+                                </Card>
+
+                                <Label label="Meta Title"><Editable value={product.metaTitle || ""} onChange={updateString("metaTitle")}/></Label>
+                                <Label label="Meta Description"><Editable textArea value={product.metaDescription || ""} onChange={updateString("metaDescription")}/></Label>
+                                <Label label="Meta Keywords"><Editable value={product.metaKeywords || ""} onChange={updateString("metaKeywords")}/></Label>
+                            </Space>
+                        </Tabs.TabPane>
+                        <Tabs.TabPane key="tags" tab="Tags">
+                            <ProductTagEditor productId={product.id} />
+                        </Tabs.TabPane>
+                        <Tabs.TabPane key="media" tab="Media">
+                            <ProductMediaEditor product={product} />
+                        </Tabs.TabPane>
+                        <Tabs.TabPane key="related" tab="Related">
+                            <RelatedProductsEditor productId={product.id} />
+                        </Tabs.TabPane>
+                        <Tabs.TabPane key="files" tab="Files">
+                            <ProductFilesEditor productId={product.id} />
+                        </Tabs.TabPane>
+                    </Tabs>
                 </Col>
             </Row>
         </>}
--- components/ProductMediaEditor/ProductMediaEditor.component.tsx
+++ components/ProductMediaEditor/ProductMediaEditor.component.tsx
@@ -2,17 +2,19 @@ import { Button, Space, Spin, Upload } from "antd";
 import {ProductMediaEditorProps} from "./ProductMediaEditor.d";
 import styles from './ProductMediaEditor.module.scss';
 import { DeleteBtn } from "@core/components/DeleteBtn";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
 
 const imgHost = (id:number) =>
-    "https:\/\/www.evilinnocence.com\/shop\/media\/catalog\/product";
-//    `https://evilinnocence.s3.us-east-1.amazonaws.com/media/product/${id}/`;
+    // "https:\/\/www.evilinnocence.com\/shop\/media\/catalog\/product";
+   `https://evilinnocence.s3.us-east-1.amazonaws.com/media/product/${id}/`;
 
 export const ProductMediaEditorComponent = ({product, media, upload, isLoading}:ProductMediaEditorProps) =>
     <Spin spinning={isLoading}>
         {!!product && <>
             <div className={styles.productMediaList}>
                 {media.map(m => <div className={styles.mediaItem}>
-                    <img src={`${imgHost(product.id)}/${m.url}`} alt={m.caption} width={256}/><br/>
+                    <img src={`${imgHost(product.id)}${m.url}`} alt={m.caption} width={256}/><br/>
                     <Space.Compact>
                         <Button type={m.id === product.thumbnailId ? "primary" : "default"} onClick={() => console.log('edit', m.id)}>Thumbnail</Button>
                         <Button type={m.id === product.mainImageId ? "primary" : "default"} onClick={() => console.log('edit', m.id)}>Main Image</Button>
@@ -24,11 +26,8 @@ export const ProductMediaEditorComponent = ({product, media, upload, isLoading}:
                 customRequest={({file}) => upload(file as File)}
                 showUploadList={false}
             >
-                <p className="ant-upload-drag-icon">
-                    <i className="anticon anticon-inbox"></i>
-                </p>
+                <FontAwesomeIcon icon={faUpload} size="3x"/><br/>
                 <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                <p className="ant-upload-hint">Support for a single or bulk upload.</p>
             </Upload.Dragger>
         </>}
     </Spin>;
