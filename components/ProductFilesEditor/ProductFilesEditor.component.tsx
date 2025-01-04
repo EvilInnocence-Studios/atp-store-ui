c6c92a03858353c5c6f5dbb7494cefb2297fab54
Andrew Wittrock <andy@wittrock.us>
2025-01-03 20:51:37 -0600

    Implement product files editor

--- components/ProductFilesEditor/ProductFilesEditor.component.tsx
+++ components/ProductFilesEditor/ProductFilesEditor.component.tsx
@@ -1,5 +1,34 @@
+import { Col, Input, Row, Spin, Upload } from "antd";
 import {ProductFilesEditorProps} from "./ProductFilesEditor.d";
 import styles from './ProductFilesEditor.module.scss';
+import { onInputChange } from "@core/lib/onInputChange";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import { DeleteBtn } from "@core/components/DeleteBtn";
 
-export const ProductFilesEditorComponent = ({}:ProductFilesEditorProps) =>
-    <div>ProductFilesEditor component goes here.</div>;
+export const ProductFilesEditorComponent = ({files, isLoading, folder, setFolder, add, remove, download}:ProductFilesEditorProps) =>
+    <Spin spinning={isLoading}>
+        <Row gutter={32} className={styles.productFilesEditor}>
+            <Col xs={6} className={styles.folder}>
+                <h2>Add new downloadable file</h2>
+                <Input addonBefore="Folder" value={folder} onChange={onInputChange(setFolder)} />
+                <Upload.Dragger
+                    name="file"
+                    multiple={false}
+                    customRequest={({file}) => add(file as File)}
+                >
+                    <FontAwesomeIcon icon={faUpload} size="3x"/><br/>
+                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                </Upload.Dragger>
+            </Col>
+            <Col xs={18} className={styles.files}>
+                <h2>Existing files</h2>
+                {files.map(file =>
+                    <div key={file.id} className={styles.file}>
+                        <a href="#" onClick={download(file)}>{file.fileName}</a>
+                        <DeleteBtn entityType="product file" onClick={remove(file.id)} />
+                    </div>
+                )}
+            </Col>
+        </Row>
+    </Spin>;
--- components/ProductFilesEditor/ProductFilesEditor.d.ts
+++ components/ProductFilesEditor/ProductFilesEditor.d.ts
@@ -1,10 +1,19 @@
-export declare interface IProductFilesEditorProps {
+import { IProductFile } from "@store-shared/product/types";
+import { Setter } from "unstateless";
 
+export declare interface IProductFilesEditorProps {
+    files: IProductFile[];
+    isLoading: boolean;
+    add: (file:File) => void;
+    remove: (fileId:number) => () => void;
+    folder: string;
+    setFolder: Setter<string>;
+    download: (file:IProductFile) => () => void;
 }
 
 // What gets passed into the component from the parent as attributes
 export declare interface IProductFilesEditorInputProps {
-
+    productId: number;
 }
 
 export type ProductFilesEditorProps = IProductFilesEditorInputProps & IProductFilesEditorProps;
