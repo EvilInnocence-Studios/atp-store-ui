3d504bc7f63b5c3ba7d6de08e56e1e4525ca119a
Andrew Wittrock <andy@wittrock.us>
2025-02-12 22:28:17 -0600

    Update image scaling on product list item

--- components/ProductListItem/ProductListItem.component.tsx
+++ components/ProductListItem/ProductListItem.component.tsx
@@ -4,14 +4,15 @@ import { Image } from "../Image";
 import { ProductPrice } from "../ProductPrice";
 import { ProductListItemProps } from "./ProductListItem.d";
 import styles from './ProductListItem.module.scss';
+import clsx from "clsx";
 
 export const ProductListItemComponent = ({product, textSize}:ProductListItemProps) =>
     <div className={styles.productListItem}>
         <Link to={`/products/${product.url}`}>
             {product.mainImageId && <Image productId={product.id} imageId={product.mainImageId} />}
         </Link>
-        <div className={styles.productInfo}>
-            <Link to={`/products/${product.url}`}><h3 className={styles[textSize || "default"]}>{product.name}</h3></Link>
+        <div className={clsx([styles.productInfo, styles[textSize || "default"]])}>
+            <Link to={`/products/${product.url}`}><h3>{product.name}</h3></Link>
         </div>
         <div className={styles.productDetails}>
             <ProductPrice product={product} />
