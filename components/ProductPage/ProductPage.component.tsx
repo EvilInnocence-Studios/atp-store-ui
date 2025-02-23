fbc2bdb9f1b730150565fe34f0567b6b8983f612
Andrew Wittrock <andy@wittrock.us>
2025-02-22 22:51:46 -0600

    Show 404 page for products that don't exist or are disabled

--- components/ProductPage/ProductPage.component.tsx
+++ components/ProductPage/ProductPage.component.tsx
@@ -8,54 +8,54 @@ import styles from './ProductPage.module.scss';
 import Markdown from 'marked-react';
 import { WishlistBtn } from "../WishlistBtn";
 import { MiniProduct } from "../MiniProduct";
+import { NotFoundPage } from "@public/components/NotFoundPage";
 
 export const ProductPageComponent = ({product, media, relatedProducts, subProducts, isLoading}:ProductPageProps) =>
     <Spin spinning={isLoading}>
-        <div className={styles.productPage}>
-            {product && <>
-                <div className={styles.header}>
-                    <h1>{product.name}</h1>
+        {!isLoading && !product && <NotFoundPage />}
+        {product && <div className={styles.productPage}>
+            <div className={styles.header}>
+                <h1>{product.name}</h1>
+            </div>
+            <div className={styles.media}>
+                <MediaSwitcher
+                    productId={product.id}
+                    media={media.filter(item => item.id !== product.thumbnailId || item.id === product.mainImageId)}
+                    defaultMediaId={product.mainImageId}
+                />
+            </div>
+            <div className={styles.productInfo}>
+                <Row className={styles.cartInfo}>
+                    <Col xs={12} className={styles.cartPrice}>
+                        <ProductPrice product={product} />
+                    </Col>
+                    <Col xs={12} className={styles.cartActions}>
+                        <AddtoCartBtn product={product} />
+                        <WishlistBtn product={product} />
+                    </Col>
+                </Row>
+                <h2>Tags</h2>
+                <div className={styles.tags}>
+                    {product.tags.map((tag) => <Tag key={tag}>{tag}</Tag>)}
                 </div>
-                <div className={styles.media}>
-                    <MediaSwitcher
-                        productId={product.id}
-                        media={media.filter(item => item.id !== product.thumbnailId || item.id === product.mainImageId)}
-                        defaultMediaId={product.mainImageId}
-                    />
-                </div>
-                <div className={styles.productInfo}>
-                    <Row className={styles.cartInfo}>
-                        <Col xs={12} className={styles.cartPrice}>
-                            <ProductPrice product={product} />
-                        </Col>
-                        <Col xs={12} className={styles.cartActions}>
-                            <AddtoCartBtn product={product} />
-                            <WishlistBtn product={product} />
-                        </Col>
-                    </Row>
-                    <h2>Tags</h2>
-                    <div className={styles.tags}>
-                        {product.tags.map((tag) => <Tag key={tag}>{tag}</Tag>)}
+                <h2>Description</h2>
+                <Markdown>{product.description}</Markdown>
+
+            </div>
+            <div className={styles.relatedProductsLists}>
+                {product.productType === 'grouped' && <>
+                    <h2>Included products</h2>
+                    <div className={styles.subProducts}>
+                        {subProducts.map(subProduct => <MiniProduct key={subProduct.id} product={subProduct} />)}
                     </div>
-                    <h2>Description</h2>
-                    <Markdown>{product.description}</Markdown>
+                </>}
 
-                </div>
-                <div className={styles.relatedProductsLists}>
-                    {product.productType === 'grouped' && <>
-                        <h2>Included products</h2>
-                        <div className={styles.subProducts}>
-                            {subProducts.map(subProduct => <MiniProduct key={subProduct.id} product={subProduct} />)}
-                        </div>
-                    </>}
-
-                    {relatedProducts.length > 0 && <>
-                        <h2>Related products</h2>
-                        <div className={styles.relatedProducts}>
-                            {relatedProducts.map((related) => <ProductListItem key={related.id} product={related} />)}
-                        </div>
-                    </>}
-                </div>
-            </>}
-        </div>
+                {relatedProducts.length > 0 && <>
+                    <h2>Related products</h2>
+                    <div className={styles.relatedProducts}>
+                        {relatedProducts.map((related) => <ProductListItem key={related.id} product={related} />)}
+                    </div>
+                </>}
+            </div>
+        </div>}
     </Spin>
