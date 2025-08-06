e89720a2494a33cd61851c8c707527a750b2b5d8
Andrew Wittrock <andy@wittrock.us>
2025-08-05 23:04:09 -0500

    Add settings for default product search

--- lib/settings.ts
+++ lib/settings.ts
@@ -16,5 +16,25 @@ export const storeSettings:ISettingContainer = {
                 description: "The folder where product images are stored.",
             },
         },
+        "Search": {
+            defaultProductSortBy: {
+                displayName: "Default Sort By",
+                type: "select",
+                defaultValue: "newest",
+                description: "The default sorting order for product listings.",
+                options: [
+                    { value: "newest", label: "Newest" },
+                    { value: "oldest", label: "Oldest" },
+                    { value: "priceLow", label: "Price: Low to High" },
+                    { value: "priceHigh", label: "Price: High to Low" },
+                ],
+            },
+            defaultProductPerPage: {
+                displayName: "Default Products Per Page",
+                type: "string",
+                defaultValue: "12",
+                description: "The default number of products displayed per page.",
+            },
+        }
     },
 };
