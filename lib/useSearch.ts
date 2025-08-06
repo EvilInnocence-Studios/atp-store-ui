b2cd19884639a3248445b9cb8f0cefcdb92c3f3f
Andrew Wittrock <andy@wittrock.us>
2025-08-05 23:08:17 -0500

    Get default search settings from settings

--- lib/useSearch.ts
+++ lib/useSearch.ts
@@ -1,3 +1,4 @@
+import { useSetting } from "@common/lib/setting/services";
 import { useNavigate, useSearchParams } from "react-router";
 import { objFilter, objMap, unique } from "ts-functional";
 import { Index } from "ts-functional/dist/types";
@@ -25,25 +26,25 @@ export declare interface ISearch {
 export const useSearch = ():ISearch => {
     const [query, setQuery] = useSearchParams();
     const navigate = useNavigate();
+    const defaultSortBy = useSetting("defaultProductSortBy");
+    const defaultPerPage = useSetting("defaultProductPerPage");
 
     const {
         q,
         tags:selectedTagIdsRaw = "",
-        sortBy = "newest",
-        perPage = "12",
+        sortBy = defaultSortBy,
+        perPage = defaultPerPage,
         page = "1",
     } = Object.fromEntries(query.entries()) as unknown as IParams;
     const selectedTagIds:string[] = selectedTagIdsRaw ? selectedTagIdsRaw.split(',') : [];
 
     const updateQuery = (params:IParams) => {
-        console.log("Updating", params);
         setQuery((old:URLSearchParams) => {
             const oldParams:IParams = Object.fromEntries(old.entries()) as unknown as IParams;
             const newParams = objMap<string, IParams>(
                 (value:any, key:keyof IParams) => params[key] || value,
             )({...oldParams, ...params});
             const filtered = objFilter(value => !! value)(newParams);
-            console.log("Updated params", old.toString(), filtered);
             return filtered as unknown as URLSearchParams;
         });        
     }
