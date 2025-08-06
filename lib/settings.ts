8f0c7d92a1affcc81c13e5eda3e301d661841dad
Andrew Wittrock <andy@wittrock.us>
2025-08-05 22:39:29 -0500

    Add default SKU setting

--- lib/settings.ts
+++ lib/settings.ts
@@ -2,7 +2,13 @@ import { ISettingContainer } from "@common/lib/setting/types";
 
 export const storeSettings:ISettingContainer = {
     "Store": {
-        "Media": {
+        "General": {
+            defaultProductSku: {
+                displayName: "Default Product SKU",
+                type: "string",
+                defaultValue: "",
+                description: "The default SKU to use for new products.",
+            },
             productImageFolder: {
                 displayName: "Product Image Folder",
                 type: "string",
