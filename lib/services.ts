f45700ace549dc2899946347c41e05436a159ac9
Andrew Wittrock <andy@wittrock.us>
2025-11-05 15:41:27 -0600

    Fix services import pathes

--- lib/services.ts
+++ lib/services.ts
@@ -1,10 +1,10 @@
 import { IMethods } from "@core/lib/types";
-import { cartServices } from "./lib/cart/services";
-import { discountServices } from "./lib/discount/services";
-import { orderServices } from "./lib/order/services";
-import { productServices } from "./lib/product/services";
-import { wishlistServices } from "./lib/wishlist/services";
-import { reportServices } from "./lib/report/services";
+import { cartServices } from "./cart/services";
+import { discountServices } from "./discount/services";
+import { orderServices } from "./order/services";
+import { productServices } from "./product/services";
+import { wishlistServices } from "./wishlist/services";
+import { reportServices } from "./report/services";
 import { donationServices } from "@donation-products-plugin/services";
 
 export const storeServices = (methods:IMethods) => ({
