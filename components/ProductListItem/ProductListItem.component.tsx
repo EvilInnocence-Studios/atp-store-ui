8ec611dda48c3676f3315b36b1f9fd7953d70bcb
Andrew Wittrock <andy@wittrock.us>
2026-04-15 23:50:32 -0500

    Convert product list item to layout

--- components/ProductListItem/ProductListItem.component.tsx
+++ components/ProductListItem/ProductListItem.component.tsx
@@ -1,30 +1,12 @@
-import clsx from "clsx";
-import { Link } from "react-router";
-import { AddToCartBtn } from "../AddToCartBtn";
-import { ProductPrice } from "../ProductPrice";
-import { ProductThumbnail } from "../ProductThumbnail";
-import { ProductListItemProps } from "./ProductListItem.d";
-import styles from './ProductListItem.module.scss';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faThumbTack } from "@fortawesome/free-solid-svg-icons";
 import { overridable } from "@core/lib/overridable";
+import { ProductIdContext } from "@store/lib/context";
+import { Layout } from "@theming/components/Layout";
+import { ProductListItemProps } from "./ProductListItem.d";
 
-export const ProductListItemComponent = overridable(({ product, textSize, hideTags, hideCartButton, classes = styles }: ProductListItemProps) =>
-    <div className={classes.productListItem}>
-        {product.pinned && <FontAwesomeIcon
-            icon={faThumbTack}
-            className={classes.pinnedIcon}
-            title="Pinned Product"
-        />}
-        <div className={classes.productMain}>
-            <ProductThumbnail product={product} />
-            <div className={clsx([classes.productInfo, classes[textSize || "default"]])}>
-                <Link to={`/products/${product.url}`}><h3>{product.name}</h3></Link>
-            </div>
-        </div>
-        <div className={classes.productDetails}>
-            <ProductPrice product={product} small={hideTags} />
-            {!hideCartButton && <AddToCartBtn id={product.id} />}
-        </div>
-    </div>
+const Provider = ProductIdContext.Provider;
+
+export const ProductListItemComponent = overridable(({ product }: ProductListItemProps) =>
+    <Provider value={product.id}>
+        <Layout element="productListItem" />
+    </Provider>
 );
