95b99607f57779c8c14ee05b8b2494ee6c2072a7
Andrew Wittrock <andy@wittrock.us>
2026-04-15 23:41:53 -0500

    Fix TypeScript error

--- components/ProductPage/ProductMediaPopup/ProductMediaPopup.layout.tsx
+++ components/ProductPage/ProductMediaPopup/ProductMediaPopup.layout.tsx
@@ -1,6 +1,6 @@
 import { ILayoutEditorProps, LayoutEditor } from "@theming/lib/layout/componentRegistry";
 
-export const ProductMediaPopupLayoutEditor:LayoutEditor = ({css, className, ...props}:ILayoutEditorProps) => <>
+export const ProductMediaPopupLayoutEditor:LayoutEditor = ({css, className}:ILayoutEditorProps) => <>
     {css && <style>{css}</style>}
     <div className={className} style={{border: '1px dashed #ccc', padding: '20px'}}>
             Product media popup goes here.
