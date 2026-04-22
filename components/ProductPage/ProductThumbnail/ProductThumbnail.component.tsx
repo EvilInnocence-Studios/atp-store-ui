500da2f4ea6ddc7e2b8b6ad76a693684d6e6537d
Andrew Wittrock <andy@wittrock.us>
2026-04-22 14:15:28 -0500

    Fix product link URLs

--- components/ProductPage/ProductThumbnail/ProductThumbnail.component.tsx
+++ components/ProductPage/ProductThumbnail/ProductThumbnail.component.tsx
@@ -6,11 +6,10 @@ import { ProductThumbnailProps } from "./ProductThumbnail.d";
 export const ProductThumbnailComponent = overridable(({className, css, product, link}:ProductThumbnailProps) => <>
     {css && <style>{css}</style>}
     {link
-        ? <Link to={`/product/${product?.name}`}>
+        ? <Link to={`/products/${product?.name}`}>
             <Image className={className} productId={product?.id || ''} imageId={product?.thumbnailId || ''} />
           </Link>
         : <Image className={className} productId={product?.id || ''} imageId={product?.thumbnailId || ''} />
     }
-    <Image className={className} productId={product?.id || ''} imageId={product?.thumbnailId || ''} />
 </>);
 
